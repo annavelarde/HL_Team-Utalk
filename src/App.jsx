835d3ada260e087c23983ed5e5286b578374016b
Anna Velarde <annavelardevilaseca@gmail.com>
2022-06-18 18:09:18 +0100

    deleting unnecessary code from react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header className="App-header">
@@ -27,7 +24,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +36,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
