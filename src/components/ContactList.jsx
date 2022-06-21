ece193ec91d463e8ba9ae4250931a8ce9cead823
juan eugenio <leuviah77@gmail.com>
2022-06-21 22:25:00 +0200

    App refactorizada, anadido el formulario el JSON

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,16 +1,18 @@
 /** @format */
 
 import React from "react";
+import { v4 as randomID } from "uuid";
 import ContactCard from "./ContactCard";
 
-const ContactList = (props) => {
-	console.log(props);
+const ContactList = ({ contacts, removeContact }) => {
+	// console.log(props);
 
-	const deleteConactHandler = (id) => {
-		props.getContactId(id);
+	const deleteContacHandler = (id) => {
+		console.log("👉 👉 / id", id);
+		removeContact(id);
 	};
-	const renderContactList = props.contacts.map((contact) => {
-		return <ContactCard contact={contact} clickHander={deleteConactHandler} key={contact.id} />;
+	const renderContactList = contacts.map((contact) => {
+		return <ContactCard contact={contact} deleteContact={deleteContacHandler} key={randomID()} />;
 	});
 	return <div className="ui celled list">{renderContactList}</div>;
 };
